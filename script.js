a3f15d1b39aebd837154168e701c52f95447d755
takazo3 <info@re-zero-jp.com>
2022-03-04 22:34:05 +0900

    first

--- script.js
+++ script.js
@@ -1,28 +1,12 @@
+const color = document.querySelector(".color");
+const btn = document.querySelector(".generate");
 
-const count = document.querySelector(".count");
-const buttons = document.querySelector(".buttons");
+const generateColor = () =>{
+  const randomColor = Math.random().toString(16).substring(2,8);
+  document.body.style.backgroundColor = "#"+ randomColor;
+  color.innerHTML = randomColor;
+}
 
-buttons.addEventListener("click", (e) =>{
-  if (e.target.classList.contains("add")){
-    count.innerHTML++;
-    setColor()
-  }
-  if (e.target.classList.contains("subtract")){
-    count.innerHTML--;
-    setColor()
-  }
-  if (e.target.classList.contains("reset")){
-    count.innerHTML =0;
-    setColor()
-  }
-});
+btn.addEventListener("click", generateColor);
 
-function setColor(){
-  if (count.innerHTML > 0){
-    count.style.color = "yellow"
-  } else if (count.innerHTML < 0){
-    count.style.color = "orangered"
-  } else {
-    count.style.color = "#fff"
-  }
-};
+generateColor();
